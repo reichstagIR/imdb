cb350e5816642cf7937e4e4bdc91799bd5913788
Reichstag <reichstagir@gmail.com>
2026-06-16 14:48:50 +0330

    add pwa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDom from "react-dom/client";
 //Lib
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 //Redux
 import store from "./redux/store";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 //Style
 import "./index.scss";
+// PWA
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
@@ -16,3 +18,5 @@ root.render(
         </Provider>
     </BrowserRouter>
 );
+
+serviceWorkerRegistration.unregister();
